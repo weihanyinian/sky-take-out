fe3c461fb27f9f7493d64946c348ddb1c4821157
saov <1012308753@qq.com>
2025-05-04 20:43:04 +0800

    店铺营业状态设置

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -6,6 +6,7 @@ import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.*;
 
@@ -34,9 +35,34 @@ public interface SetmealMapper {
 
     @Select("select * from setmeal where id = #{id}")
     Setmeal getById(Long id);
+
+    /**
+     *  根据id删除套餐
+     * @param setmealId
+     */
     @Delete("delete from setmeal where id = #{id}")
     void deleteById(Long setmealId);
 
+    /**
+     * 根据id修改套餐
+     * @param setmeal
+     */
     @AutoFill(OperationType.UPDATE)
     void update(Setmeal setmeal);
+    /**
+     * 动态条件查询套餐
+     * @param setmeal
+     * @return
+     */
+    List<Setmeal> list(Setmeal setmeal);
+    /**
+     * 根据套餐id查询菜品选项
+     * @param setmealId
+     * @return
+     */
+    @Select("select sd.name, sd.copies, d.image, d.description " +
+            "from setmeal_dish sd left join dish d on sd.dish_id = d.id " +
+            "where sd.setmeal_id = #{setmealId}")
+    List<DishItemVO> getDishItemBySetmealId(Long setmealId);
+
 }
